858a89bf927711d6087f99b3bf56bf70f8bebaeb
Thoura Gitlab <laithoura.web@gmail.com>
2022-01-23 15:47:26 +0700

    feat: init spring boot microservice project

---
+++ customer/src/main/java/com/thouradev/customer/CustomerController.java
@@ -0,0 +1,18 @@
+package com.thouradev.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class CustomerController {
+
+  @GetMapping(value = {"", "/message"})
+  public String getMessage(@RequestParam(name = "message", required = false, defaultValue = "Docker") String message) {
+    log.info("method=getMessage message={}", message);
+    return String.format("Customer: Hello from %s!", message);
+  }
+
+}
---
+++ message/src/main/java/com/thouradev/message/MessageController.java
@@ -0,0 +1,21 @@
+package com.thouradev.message;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class MessageController {
+
+  @GetMapping(value = {"", "/message"})
+  public String getMessage(@RequestParam(name = "message", required = false, defaultValue = "Docker") String message) {
+
+
+    log.info("method=getMessage message={}", message);
+    return String.format("Message: Hello from %s!", message);
+
+  }
+
+}
